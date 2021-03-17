18d261182f76639e573f428dad036392fbd76249
Dmitry Zolotyh <zolotyh.dk@gmail.com>
2021-03-17 12:54:39 +0300

    Управжнение: настройка уведомлений

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,18 +22,22 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
             }
         });
     }
