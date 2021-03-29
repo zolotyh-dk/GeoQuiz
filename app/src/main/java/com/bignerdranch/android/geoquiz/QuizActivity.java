edab593dc9d4a80374bbef73b2215bbb49c08900
Dmitry Zolotyh <zolotyh.dk@gmail.com>
2021-03-29 16:20:45 +0300

    Упражнение: вывод оценки

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -14,7 +14,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private final static String TAG = "QuizActivity";
     private final static String KEY_INDEX = "index";
-    
+    private final static String KEY_CORRECT = "correct";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -29,6 +30,7 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true),
     };
     private int mCurrentIndex = 0;
+    private int mNumberCorrectAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mNumberCorrectAnswers = savedInstanceState.getInt(KEY_CORRECT, 0);
         }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -83,10 +86,18 @@ public class QuizActivity extends AppCompatActivity {
         int messageResId = 0;
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
+            mNumberCorrectAnswers++;
         } else {
             messageResId = R.string.incorrect_toast;
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
+        if (mCurrentIndex == mQuestionBank.length - 1) {
+            int percent = 100 * mNumberCorrectAnswers / mQuestionBank.length;
+            String message = "Percentage of correct answers is " + percent;
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            mNumberCorrectAnswers = 0;
+        }
     }
 
     private void lockButtons() {
@@ -122,6 +133,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         Log.i(TAG, "onSaveInstanceState() called");
         outState.putInt(KEY_INDEX, mCurrentIndex);
+        outState.putInt(KEY_CORRECT, mNumberCorrectAnswers);
     }
 
     @Override
